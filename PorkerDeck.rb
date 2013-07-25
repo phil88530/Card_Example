c234472634dda35ca6213d9f2741d9be10e0247a
YuFei Zhu <phil88530@gmail.com>
2013-07-25 21:59:51 +0100

    made definations for cards

--- PorkerDeck.rb
+++ PorkerDeck.rb
@@ -52,17 +52,6 @@ class PokerHand
     @cards.sort!{|a,b| a.card_value <=> b.card_value }
   end
 
-
-  def is_flush
-    is_flush = false
-    for i in 1..(@cards.count - 1)
-      puts i
-      is_flush = (@cards[i].card_value - @cards[i-1].card_value == 1)
-    end
-
-    is_flush
-  end
-
   #order cards
   def ordered_cards
     card_hash = Hash.new(0)
@@ -72,9 +61,59 @@ class PokerHand
 
     card_hash
   end
+
+  #methods for getting card kinds
+  def is_straight_flush?
+    is_straight_flush = true
+    for i in 1..(@cards.count - 1)
+      is_flush = false if (@cards[i].card_value - @cards[i-1].card_value == 1) && (@cards[i].suit == @cards[i-1].suit)
+    end
+
+    is_straight_flush
+  end
+
+  def is_four_of_a_kind?
+    ordered_cards.has_value?(4)
+  end
+
+  def is_full_house?
+    od = ordered_cards
+    od.has_value?(3) && od.has_value?(2) 
+  end
+
+  def is_flush?
+    is_flush = true
+    for i in 1..(@cards.count - 1)
+      is_flush = false if (@cards[i].suit == @cards[i-1].suit)
+    end
+
+    is_flush
+  end
+
+  def Straight?
+    is_straight = true
+    for i in 1..(@cards.count - 1)
+      is_flush = false if (@cards[i].card_value - @cards[i-1].card_value == 1)
+    end
+
+    is_straight
+  end
+
+  def is_three_of_a_kind?
+    ordered_cards.has_value?(3)
+  end
+
+  def is_two_pairs?
+    ordered_cards.values.count(2) == 2
+  end
+
+  def is_pair?
+    ordered_cards..values.count(2) == 1
+  end
 end
 
 d = Deck.new
 p = PokerHand.new(d.cards.sample(5))
 puts "Getting cards:" + p.cards.to_s
 puts "A hand has values:" + p.ordered_cards.to_s
+
